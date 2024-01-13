fd0b1ebea510b64abeb1692d4fcd7f2c68295c8c
MaxBro <activer12@mail.ru>
2024-01-14 00:22:43 +0300

    start: lets start work on this

---
+++ Bacteria_Network/__main__.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+
+def main(args: list):
+    pass
+
+
+if __name__ == '__main__':
+    try:
+        main(argv)
+    except Exception:
+        pass
+
---
+++ Bacteria_Network/core/__init__.py
@@ -0,0 +1,3 @@
+from .debug import create_log, log_decorator
+from .exceptions import OsException, ConfigException
+
